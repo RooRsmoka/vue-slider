9da7077780ada2c65db0ed5668fc24e20faec84a
RooRsmoka <chri.disa96@gmail.com>
2022-01-14 19:37:44 +0100

    creazione slider

--- js/script.js
+++ js/script.js
@@ -4,11 +4,27 @@ new Vue({
         heading: 'Best photographer ever EU',
         currentIndex: 0,
         images: [
-            'https://unsplash.it/600/400?image=586',
-            'https://unsplash.it/600/400?image=96',
             'https://unsplash.it/600/400?image=666',
             'https://unsplash.it/600/400?image=249',
-            'https://unsplash.it/600/400?image=413'
+            'https://unsplash.it/600/400?image=456',
+            'https://unsplash.it/600/400?image=386',
+            'https://unsplash.it/600/400?image=287'
         ],
     },
+    methods: {
+        // Funzione che avanza di 1 ogni volta che si clicca sul "Pulsante".
+        nextImage: function() {
+            this.currentIndex++
+            if ( this.currentIndex > this.images.length - 1) {
+                this.currentIndex = 0;
+            }
+        },
+        // Funzione che torna indietro di 1 ogni volta che si clicca sul "Pulsante".
+        prevImage: function() {
+            this.currentIndex--
+            if ( this.currentIndex < 0) {
+                this.currentIndex = this.images.length - 1;
+            }
+        },
+    }
 })
