e839c7acbd70cc3212e77cdc202cc03112f6f61c
RooRsmoka <chri.disa96@gmail.com>
2022-01-14 18:14:01 +0100

    inserito lista immagini nel data, stampa in pagina delle foto e creazione regole css

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
 new Vue({
     el: '#app',
+    data: {
+        heading: 'Best photographer ever EU',
+        currentIndex: 0,
+        images: [
+            'https://unsplash.it/600/400?image=586',
+            'https://unsplash.it/600/400?image=96',
+            'https://unsplash.it/600/400?image=666',
+            'https://unsplash.it/600/400?image=249',
+            'https://unsplash.it/600/400?image=413'
+        ],
+    },
 })
